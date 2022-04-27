90d2cbaa75c78a4cd66ebb6a41687add67d61a04
mauroue <mauroue@gmail.com>
2022-04-27 11:48:02 -0300

    Finalizado curso Alura Node.JS e mongoDB

--- alura-node/server.js
+++ alura-node/server.js
@@ -1,21 +1,8 @@
-const http = require("http")
-const port = 3000;
+import app from './src/app.js'
+
+const port = process.env.PORT || 3000;
 
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na pag de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'pagina de editora',
-    '/sobre': 'info sobre o projeto'
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Curso de Node');
-})
-
-
-server.listen(port, () => {
-    console.log(`Servidor escutando em http:localhost:${port}`)
+app.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`)
 })
