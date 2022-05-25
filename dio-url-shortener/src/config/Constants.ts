9dbb2f03438c855449b8ff2d40ffcc831e22f0cd
Mauro Ue <mauroue@gmail.com>
2022-05-24 22:58:44 -0300

    Update Constants.ts

--- dio-url-shortener/src/config/Constants.ts
+++ dio-url-shortener/src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
     API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: "mongodb+srv://admin:S3yXoDI6evjKKyJY@url-shortener-data.kqyd7vy.mongodb.net/url-shortener?retryWrites=true&w=majority"
+    MONGO_CONNECTION: "mongodb_url"
 }
