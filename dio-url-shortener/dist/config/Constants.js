0cba5d3ea96758c05adb5eebb6c937d6d608d6cd
mauroue <mauroue@gmail.com>
2022-05-24 22:45:57 -0300

    added dio url shortener for spread bootcamp 2022

---
+++ dio-url-shortener/dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: "mongodb+srv://admin:S3yXoDI6evjKKyJY@url-shortener-data.kqyd7vy.mongodb.net/url-shortener?retryWrites=true&w=majority"
+};
+//# sourceMappingURL=Constants.js.map
